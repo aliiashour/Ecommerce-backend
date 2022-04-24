ed6b37af6d58dd4a2bfe1bdf3690ea31aa6366a5
aliiashour <aliashour592@gmail.com>
2022-04-24 20:48:03 +0200

    modifiy complete

--- admin/include/language/en.php
+++ admin/include/language/en.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php
 //en.php
 function lang($phase){
@@ -55,65 +55,3 @@ function lang($phase){
 
 }
 ?>
-
-
-=======
-<?php
-//en.php
-function lang($phase){
-
-    static $lang = array(
-        // login page
-        'LOGINFORM'     => 'Login Form',
-        'USER'          => 'User Name',
-        'ENTERNAME'     => 'Enter Your Name',
-        'CONNAME'       => 'Please Make Sure That Your Name Is Correct :D',
-        'PASS'          => 'Password',
-        'ENTERPASS'     => 'Enter Your Password',
-        'CONPASS'       => 'Please Make Sure That Your Password Is More Than 8 Characters !',
-        'LOGIN'         => 'Login',
-        //navbar page
-        'HOME'          => 'Home',
-        'SECTIONS'      => 'Categores',
-        'ITEMS'         => 'Items',
-        'MEMBERS'       => 'Members',
-        'COMMENTS'      => 'Comments',
-        'STATISTICS'    => 'Statistics',
-        'LOGS'          => 'Logs',
-//        'AdmainName'    => $_SESSION["admin"],
-        'AdmainName'    => 'Ali',
-        'EDITPROFILE'   => 'Edit Profile',
-        'SETTINGS'      => 'Settings',
-        'LOGOUT'        => 'Logout',
-        // Manage Member Page
-        'MANAGE'        => 'Manage Members',
-        // Add Member Page
-        'ADD'           => 'This Is Add Page',
-        'ADDMEMBER'     => 'Add New Member',
-        'ADDONLY'       => 'Add',
-        'SUCCESSADD'    => 'Add Process Completed',
-        // Edit Members page
-        'USERNAME'      => 'Username',
-        'PASSWORD'      => 'Password',
-        'EMAIL'         => 'Email',
-        'FULLNAME'      => 'Fullname',
-        'SAVE'          => 'Save',
-        'CEUP'          => 'Sorry , You Are Not Allowed To Be Here :DD', // CANT ENTER UPDATE PAGE
-        'UPDATE'        => 'Update',
-        'BACK'          =>'Back',
-        'EDITMEMBERS'   => 'Edit Members',
-        'UPDATEMEMBERS' => 'Update Members',
-        'CANTUPDATE'    => 'Sorry , You Should Enter All Fields In Form Or Password Should Be More Than 7 Characters',
-        'SUCCESSUPDATE' => 'Success , Data Successfully Updated',
-        'CHECK'         => 'Check Data',
-        'Re-Enter'      => "Re-Enter Data",
-        'ERROR'         => 'Back'
-    );
-    
-    return $lang[$phase] ; 
-
-}
-?>
-
-
->>>>>>> 91fbde6ee816f38ea5056f286dae4e467b97dba1
--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 // index.php
 session_start() ; 
@@ -59,70 +58,4 @@ if($_SERVER["REQUEST_METHOD"] == "POST" && strlen($_POST["user"]) > 0 && strlen(
     <p class="lead" id="response"></p>
 </form>
 
-
-
-=======
-<?php
-// index.php
-session_start() ; 
-$nonav = '' ;
-$pageTitle = "Login" ;
-//$ar='' ;
-if(isset($_SESSION["admin"])){
-    header("location:dashbord.php") ;
-}
-include "init.php" ;
-
-if($_SERVER["REQUEST_METHOD"] == "POST" && strlen($_POST["user"]) > 0 && strlen($_POST["password"]) >= 8 )
-{
-    $name = $_POST["user"];
-    $pass = $_POST["password"];
-    $hashedpass = sha1($pass) ;
-    $stmt = $con->prepare("SELECT 
-                                userId, userName, password 
-                           FROM 
-                                users 
-                           WHERE 
-                                userName = ? 
-                           AND 
-                                password = ? 
-                           AND 
-                                groupId = 1 
-                           LIMIT 1") ;
-    $stmt->execute(array($name, $hashedpass)) ;
-    $row = $stmt->fetch() ; 
-    $res = $stmt->rowCount()  ;
-    if($res)
-    {
-    	$_SESSION["admin"] = $name ;
-    	$_SESSION["adminId"] = $row['userId'];
-    	header("location:dashbord.php") ; 
-    }
-}
-?>
-
-
-
-<form class="login-form" action="<?php echo $_SERVER["PHP_SELF"] ?>" method="POST">
-    <h2 class="h1 text-center"><?php echo lang('LOGINFORM') ?></h2>
-    <div class="form-group">
-        <label><?php echo lang('USER') ?></label>
-        <input class="form-control" type="text" name="user" placeholder="<?php echo lang('ENTERNAME') ?>" autocomplete="on">
-        <small><?php echo lang('CONNAME') ?></small>
-    </div>
-    
-    <div class="form-group">
-        <label><?php echo lang('PASS') ?></label>
-        <input class="form-control" type="password" name="password" placeholder="<?php echo lang('ENTERPASS') ?>" autocomplete="on">
-        <small><?php echo lang('CONPASS') ?></small>
-    </div>
-    
-    <button class="btn btn-primary float-right"><?php echo lang('LOGIN') ?></button>
-    <button class="btn btn-primary float-left"><a href="/ECO/" style="color:#FFF;text-decoration:#FFF" ><?php echo lang('BACK') ?></a></button>
-    <p class="lead" id="response"></p>
-</form>
-
-
-
->>>>>>> 91fbde6ee816f38ea5056f286dae4e467b97dba1
 <?php include $tmpls.'footer.php' ; ?>
